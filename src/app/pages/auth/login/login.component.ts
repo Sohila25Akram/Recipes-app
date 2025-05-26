45c8e501864183ddb18e48ebb440e1055335388c
Sohila25Akram <sohilaakram305@gmail.com>
2025-05-27 00:19:39 +0300

    login validation

--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -1,12 +1,15 @@
-import { ChangeDetectionStrategy, Component, DestroyRef, inject, OnDestroy, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, DestroyRef, inject, OnDestroy, Signal, signal } from '@angular/core';
 import { AuthService } from '../../../services/auth.service';
 import { ReactiveFormsModule, FormGroup, FormControl } from '@angular/forms';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { catchError, map, of, Subject, takeUntil } from 'rxjs';
+import { error } from 'console';
+import { HttpErrorResponse } from '@angular/common/http';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-login',
-  imports: [ReactiveFormsModule, RouterLink],
+  imports: [ReactiveFormsModule, RouterLink, MatIconModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
@@ -15,9 +18,11 @@ export class LoginComponent implements OnDestroy{
   private authService = inject(AuthService);
   // private destroyRef = inject(DestroyRef);
   private destroy$ = new Subject<void>();
+  private router = inject(Router)
 
   isLoggged = signal(false);
   currentForm : 'Login' | 'Signup' = 'Signup';
+  isError = signal(false);
 
   form = new FormGroup({
     username: new FormControl(),
@@ -36,15 +41,17 @@ export class LoginComponent implements OnDestroy{
       takeUntil(this.destroy$),
       map((res) => {
         console.log('Login response:', res);
-        // localStorage.setItem('userData', JSON.stringify(res));
         this.isLoggged.set(true);
-        return res;
       }),
-      catchError((err) => {
-        console.log('Login error:', err);
-        return of([]);
-      })
-    ).subscribe();
+    ).subscribe({
+      next: ()=> this.router.navigateByUrl('/'),
+      error: (err: HttpErrorResponse) => {
+        if(err.status === 400){
+          this.isError.set(true);
+          console.error('invalid credentials')
+        }
+      }
+    });
 
     // this.destroyRef.onDestroy(() => sup.unsubscribe())
   }
